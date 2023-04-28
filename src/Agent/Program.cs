f1b9ddfc31cf09d9a091c95606668588ae5b1073
Antoni <antekkrol1@gmail.com>
2023-04-28 15:33:29 +0200

    Connection to IoT Hub - Device

--- src/Agent/Program.cs
+++ src/Agent/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DeviceSdkAgent.Device;
+using Microsoft.Azure.Devices.Client;
+
+string deviceConnectionString = "HostName=Zajecia-IOT.azure-devices.net;DeviceId=Industrial_IoT;SharedAccessKey=9fVTEZ1Bd+3AaxvU/tvLTL+02KnCJr3YIRPTU2srawk=";
+
+using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+
+await deviceClient.OpenAsync();
+
+var device = new VirtualDevice(deviceClient);
+
+Console.WriteLine("Connected");
---
+++ src/DeviceSdkAgent.Device/VirtualDevice.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Devices.Client;
+
+namespace DeviceSdkAgent.Device
+{
+    public class VirtualDevice
+    {
+        private readonly DeviceClient deviceClient;
+
+        public VirtualDevice(DeviceClient deviceClient)
+        {
+            this.deviceClient = deviceClient;
+        }
+    }
+}
